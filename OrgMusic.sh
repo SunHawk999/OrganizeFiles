61e727512f4616b3e7e137d6a1fb86d0fefdc40b
ethan <ethanw123@gmail.com>
2019-12-16 16:12:59 -0500

    Clean up code, begin working on directory searching and manipulation

--- OrgMusic.sh
+++ OrgMusic.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
 
+#Create some functions here to declutter code
+# $1 will be file name
+# $2 artist_name
+# $3 album_name
 
-MUSICDIREC="$1" 	#Save the first argument as "$musDirectory" 
-CURREDIREC="$(pwd)" #Save the current working directory
+artist_exists () {
+	echo "$2" directory exists! Attempting to move "$1" into "$2"/
+	mv "$1" "$2/"
+}
 
-cd $MUSICDIREC
+artist_does_not_exist () {
+	echo "$2" does not exist! Attempting to make directory...
+	mkdir "$2"
+	echo "$2"/ made! Moving "$1" into "$2"/
+	mv "$1" "$2/"
+}
+
+look_for_mp3_files () {
 
 for file in *.mp3 ; do
-	#echo "$file"
-	#testVar="$(mp3info -p "%a" "$file")"
-	#echo $testVar
-
 	
 	# Subshell for scoping
 	(
-	
 	artist_name="$(mp3info -p "%a" "$file")"
 	album_name="$(mp3info -p "%l" "$file")"
 
-	#echo $albumName	
 	# Perform a check if the artist directory exists
 	if [[ -d  "$artist_name/" ]] ; then
-		echo "$artist_name" directory exists! Attempting to move "$file" into "$artist_name"/
-		mv "$file" "$artist_name/"
-
-
-	else 
-		echo "$artist_name" does not exist! Attempting to make directory...
-		mkdir "$artist_name"
-		echo "$artist_name"/ made! Moving "$file" into "$artist_name"/
-		mv "$file" "$artist_name/"
+		artist_exists "$file" "$artist_name"
 		
+		#change directory to artist
+		
+	elif [[ ! -d "$artist_name/" ]] ; then 
+		artist_does_not_exist "$file" "$artist_name"
 		
 	fi
 	)
 		
+done
+} 
+
+MUSICDIREC="$1"				#Save the first argument as "$MUSICDIREC" 
+CURREDIREC="$(pwd)"		#Save the current working directory
+
+cd $MUSICDIREC
+
+echo $MUSICDIREC 
+
+#look for mp3 files first
+look_for_mp3_files
+
+#move onto directory search
+for direc in */ ; do
+	echo "$direc"
 done
 
 cd $CURREDIREC
