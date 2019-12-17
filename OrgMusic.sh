4ca33a2a410e2217e02aae49d1fd01d24f8eb671
ethan <ethanw123@gmail.com>
2019-12-17 14:55:20 -0500

    Clean up code, create and finish look_for_mp3_files_album function. Should be able to Organize mp3 files relatively well.

--- OrgMusic.sh
+++ OrgMusic.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
-#TODO: Need to rething how files and directories are going to be organized...
+#TODO: Need to rethink how files and directories are going to be organized...
 
 #Create some functions here to declutter code
 # $1 will be file name
-# $2 artist_name
-# $3 album_name
+# $2 artist_name or album_name depending on particular function
 
 artist_exists () {
 	echo "$2" directory exists! Attempting to move "$1" into "$2"/
@@ -14,8 +12,8 @@ artist_exists () {
 }
 
 artist_album_exists () {
-	echo "$2"/"$3" directory exists! Attempting to move "$3"/ into "$2"/
-	mv "$2" "$3" 
+	echo "$1"/"$2" directory exists! Attempting to move "$1"/ into "$2"/
+	mv "$1" "$2" 
 }
 
 artist_does_not_exist () {
@@ -26,38 +24,66 @@ artist_does_not_exist () {
 }
 
 #This might not work, hasn't been tested yet...
-artist_album_does_not_exists () {
-	echo "$2"/"$3" does not exist! Attempting to make directory...
-	cd "$MUSICDIREC"/"$2" 	
-	mkdir "$3"
+artist_album_does_not_exist () {
+	echo "$1"/"$2" does not exist! Attempting to make directory...
+	cd "$MUSICDIREC"/"$1" 	
+	mkdir "$2"
 	cd "$MUSICDIREC" 
 }
 
-look_for_mp3_files () {
+look_for_mp3_files_artist () {
 
-for file in *.mp3 ; do
+	for file in *.mp3 ; do	
+		# Subshell for scoping
+		(
+		artist_name="$(mp3info -p "%a" "$file")"
+		
+		# Perform a check if the artist directory exists
+		if [[ -d  "$artist_name/" ]] ; then
+			artist_exists "$file" "$artist_name"
+		
+			#change directory to artist
+		
+		elif [[ ! -d "$artist_name/" ]] ; then 
+			artist_does_not_exist "$file" "$artist_name"
+		
+		fi
+		)
+		
+	done
+}
+
+look_for_mp3_files_album () {
 	
-	# Subshell for scoping
-	(
-	artist_name="$(mp3info -p "%a" "$file")"
-	album_name="$(mp3info -p "%l" "$file")"
+	for file in *.mp3 ; do
+		#Subshell for scoping
+		(
+		album_name="$(mp3info -p "%l" "$file")"
+			
+		if [[ -d "$album_name/" ]] ; then
+			artist_album_exists "$file" "$album_name"
+	
+		elif [[ ! -d "$album_name/" ]] ; then
+			artist_album_does_not_exist "$file" "$album_name"
 
-	# Perform a check if the artist directory exists
-	if [[ -d  "$artist_name/" ]] ; then
-		artist_exists "$file" "$artist_name"
-		
-		#change directory to artist
-		
-	elif [[ ! -d "$artist_name/" ]] ; then 
-		artist_does_not_exist "$file" "$artist_name"
-		
-	fi
-	)
-		
-done
+		fi 
+		)
+
+	done
 } 
 
-#look_for_directories () {}
+look_for_directories () {
+	
+	for direc in */ ; do
+		[ -d "${direc}" ] || continue		# if not a directory, skip
+		#echo "${direc}"
+		cd "${direc}"
+		#echo $pwd
+		look_for_mp3_files_album 
+		cd ./..
+		
+	done
+}
 
 MUSICDIREC="$1"				#Save the first argument as "$MUSICDIREC" 
 CURREDIREC="$(pwd)"		#Save the current working directory
@@ -68,19 +94,10 @@ echo $MUSICDIREC
 
 #look for mp3 files first
 echo Looking for stray mp3 files...
-look_for_mp3_files
+look_for_mp3_files_artist
 
 #move onto directory search
 echo Now looking through directories...
-for direc in */ ; do
-	echo "$direc"
-	cd "$direc"
-
-	#(
-	
-	#)	
-	cd ./..
-
-done
+look_for_directories
 
 cd $CURREDIREC
