f78a947f20371d0060a072b8adf13fef2d87a1d8
Ethan <ethanw123@gmail.com>
2020-05-02 08:54:35 -0400

    Create CopyMusicOut bash script to cp music to a specified directory. Usually use it to send files to a jump or flash drive. Works better than most linux GUI file systems.

---
+++ CopyMusicOut.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Create some functions here to declutter code
+# $1 will be source directory
+# $2 will be destination directory, probably jumpdrives and such
+
+#Pattern:
+#   Look for music files, then cp them to destination
+#   Look for directories, if no directories found, stop 
+#       Go into found directory, 
+#           Look for music files and cp them to destination
+#           Look for directories, if no directories found, go back
+
+copy_music_file () {
+    
+    for file in *.m* ; do
+        printf "${file}"
+        printf "Copying ${file} to ${DESTINATIONDIREC}"
+        cp "$file" "$DESTINATIONDIREC"
+    done
+}
+
+look_for_directories () {
+    
+    copy_music_file
+	for direc in */ ; do
+		[ -d "${direc}" ] || continue		# if not a directory, skip
+        cd "${direc}"
+        #pwd
+        look_for_directories
+		cd ./..	
+	done
+}
+
+CURREDIREC="$(pwd)"		#Save the current working directory
+SOURCEDIREC="$1"
+DESTINATIONDIREC="$2"
+
+#When a pattern matches nothing "Disappears", rather than being treated 
+#as a literal string
+shopt -s nullglob
+cd $SOURCEDIREC
+
+look_for_directories
+
+cd $CURREDIREC
